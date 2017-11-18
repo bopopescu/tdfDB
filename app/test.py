66c3ded14c27e1f75b1a04ec12bf59d29a0037e7
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2017-11-18 00:29:43 -0500

    Working in HTML/CSS template

--- app/test.py
+++ app/test.py
@@ -27,13 +27,39 @@ def hello_world():
 	option_list = cur.fetchall()    
 
 
-	return render_template('index.html', option_list= option_list)
+	return render_template('blog-simple	.html', option_list=option_list)
 
 
 
-
-@app.route("/forward/", methods=['POST'])
+@app.route("/forward/", methods=["GET", "POST"])
 def move_forward():
-    #Moving forward code
-    forward_message = "Moving Forward..."
-    return render_template('blog.html');
+
+	x = ""
+	if request.method == 'POST':
+		print "POST METHOD"
+		x = request.form.get("teamdata")
+		x = "*"
+	elif request.method == "GET":
+		print "GET METHOD"
+	
+
+
+	cur = db.cursor()
+	query = "SELECT DISTINCT {0} FROM tdf.cyclist LIMIT 10".format(x)
+	print query
+	
+
+	query = """ SELECT stageNum as stageNumber, Team, sec_to_time(avg(time_to_sec(stageTime))) as time
+	from tdf.cyclist c1
+	left join tdf.competes c2 on c1.Name = c2.Name
+	group by stageNum, Team
+	order by stageNumber asc, time asc """
+	cur.execute(query)
+
+	tableDat = cur.fetchall()
+	columns = [desc[0] for desc in cur.description]
+	
+
+
+	return render_template('results.html', tableDat=tableDat, columns=columns)
+
